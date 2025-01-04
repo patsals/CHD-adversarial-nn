6d5df9add78b9a10932f25ef04b7cacd998ec65f
Patrick Salsbury <patrick.salsbury@yahoo.com>
2025-01-04 11:40:51 -0800

    finalized data etl pipeline, began eda notebook

--- data_setup.py
+++ data_setup.py
@@ -6,9 +6,20 @@ import json
 
 # Configurations
 webpage_url = 'https://wwwn.cdc.gov/nchs/nhanes/search/datapage.aspx?Component={}&CycleBeginYear={}'
-data_attributes_file_path = "data_attributes.json"
+config_directory = "config"
+data_directory = 'data'
+data_attributes_file_name = "data_attributes.json"
+attribute_name_map_file_name = 'data_attribute_names_map.json'
+dataset_save_file_name = 'processed_nhanes_data.parquet'
+file_directories_file_name = "file_directories.json"
+
+file_directories_file_path = os.path.join(config_directory, file_directories_file_name)
+data_attributes_file_path = os.path.join(config_directory, data_attributes_file_name)
+attribute_name_map_file_path = os.path.join(config_directory, attribute_name_map_file_name)
+dataset_save_file_path = os.path.join(data_directory, dataset_save_file_name)
+
 start_years = [1999, 2001, 2003, 2005, 2007, 2009, 2011, 2013, 2015]
-output_directory = 'data_test'
+
 
 def download_specific_files(url, output_folder, filename):
     """
@@ -63,8 +74,7 @@ def download_files_by_year_range(output_directory, year_range):
         year_range (str): The year range of data to download
     """
     start_year = int(year_range.split('-')[0])
-    file_directories_path = 'file_directories.json'
-    with open(file_directories_path, "r") as file:
+    with open(file_directories_file_path, "r") as file:
         file_directory_dictionary = json.load(file)
 
     for directory, files in file_directory_dictionary[year_range].items():
@@ -88,7 +98,7 @@ def validate_attributes(directory, year_range):
         directory (str): The parent directory where all the data files are located.
         year_range (str): The year range of data to validate
     """
-    print(f'Checking data extracted from year range: {year_range}')
+    print(f'Checking data extracted from year range')
 
     # Read the dictionary from the JSON file
     with open(data_attributes_file_path, "r") as file:
@@ -114,14 +124,69 @@ def validate_attributes(directory, year_range):
         print(message.rstrip('\n'))
 
 
-for start_year in start_years:
-    year_range = str(start_year) + '-' + str(start_year + 1)
-    print(f'~~~~EXTRACTING {year_range} DATA~~~~')
+def process_attributes(directory, year_range):
+    """
+    Processes the dataset by extracting and renaming the desired columns.
+    
+    Args:
+        directory (str): The parent directory where all the data files are located.
+        year_range (str): The year range of data to validate
+    """
+    print(f'Processing data extracted from year range')
 
-    output_file_path = f'{output_directory}/{year_range}'
-    download_files_by_year_range(output_file_path, year_range)
+    with open(attribute_name_map_file_path, "r") as file:
+        attribute_name_map = json.load(file)
+
+    with open(data_attributes_file_path, "r") as file:
+        data_attribute_dict = json.load(file)
+
+    result_df = pd.DataFrame({'SEQN': []})
+    for file_name in os.listdir(directory):
+        data_file_path = os.path.join(directory, file_name)
+        file_df = pd.read_sas(data_file_path, format='xport')
+
+        # Rename columns to common naming convention
+        extracted_columns = file_df.columns.intersection(data_attribute_dict[year_range])
+        extracted_columns = list(extracted_columns) + ['SEQN']
+        print(f' - Length of dataset extracted from {file_name}: ', len(file_df))
+
+        df_i = file_df[extracted_columns]
+        df_i = df_i.rename(columns=attribute_name_map)
+
+        result_df = result_df.merge(df_i, how='outer', on='SEQN')
+
+    print(' - Merging all Datasets [SUCCESS]')
+    return result_df
+
+
+
+if __name__ == "__main__":
+    dfs = []
+
+    for start_year in start_years:
+        year_range = str(start_year) + '-' + str(start_year + 1)
+        print(f'~~~~~~~~~~~~~~~~ EXTRACTING {year_range} DATA ~~~~~~~~~~~~~~~~')
+
+        output_file_path = f'{data_directory}/{year_range}'
+        download_files_by_year_range(output_file_path, year_range)
+        print()
+
+        validate_attributes(output_file_path, year_range)
+        print()
+
+        df_i = process_attributes(output_file_path, year_range)
+        dfs.append(df_i)
+
+        print(f'~~~~~~~~~~~~~~~~~~~~~~~~~~~ DONE. ~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n')
+
+
+    print(f'~~~~~~~~~~~~~~~~~~~~ SAVING ENTIRE DATA ~~~~~~~~~~~~~~~~~~~~~~')
+    print('Appending all dataset years ', end="")
+    processed_df = pd.concat(dfs)
+    print('[SUCCESS]')
     print()
-    validate_attributes(output_file_path, year_range)
+    print(f'Saving total combined dataset to: {dataset_save_file_path}', end="")
+    processed_df.to_parquet(dataset_save_file_path)
+    print(' [SUCCESS]')
 
-    print(f'~~~~DONE.~~~~\n')
-
+    print(f'~~~~~~~~~~~~~~~~~~~~~~~~ COMPLETE. ~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
