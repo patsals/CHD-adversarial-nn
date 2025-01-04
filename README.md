eaca92f824f2f653384277ea8937ef15388b9e8a
Patrick Salsbury <patrick.salsbury@yahoo.com>
2025-01-03 23:26:00 -0800

    made some edits and added a simple readme

---
+++ README.md
@@ -0,0 +1,36 @@
+# DSC180A - Capstone Project
+
+This repository contains all of the code that was utilized for the 2024-2025 UCSD Data Science Capstone Project.
+
+---
+## Introduction
+
+
+---
+## How to get Started
+
+##### 1) Getting the code
+- clone repository into your working directory:
+    `git clone https://github.com`
+
+##### 2.a) Setting up environment (Windows Users)
+- create a working environment:
+    `python -m venv venv`
+- activate the working environment:
+    `venv/scripts/activate`
+- download the dependencies:
+    `pip install -r requirements.txt`
+
+##### 2.b) Setting up environment (Linux/Mac OS users)
+- create a working environment:
+    `python3 -m venv venv`
+- activate the working environment:
+    `source venv/bin/activate`
+- download the dependencies:
+    `pip install -r requirements.txt`
+
+##### 3) Downloading the Data
+- Run the data setup file to extract, transform, and load all of the data locally:
+    `python data_setup.py` for windows users
+    `python3 data_setup.py` for Linux/Mac OS users
+- Note: The script makes requests to multiple url endpoints at www.sciencedirect.com to download the files. It is important to lookout for any message logs that dont show `[SUCCESS]` as an output — this indicates an error that needs to be resolved.
--- data_setup.py
+++ data_setup.py
@@ -1,10 +1,14 @@
 import pandas as pd
-
 import os
 import requests
 from bs4 import BeautifulSoup
 import json
 
+# Configurations
+webpage_url = 'https://wwwn.cdc.gov/nchs/nhanes/search/datapage.aspx?Component={}&CycleBeginYear={}'
+data_attributes_file_path = "data_attributes.json"
+start_years = [1999, 2001, 2003, 2005, 2007, 2009, 2011, 2013, 2015]
+output_directory = 'data_test'
 
 def download_specific_files(url, output_folder, filename):
     """
@@ -50,58 +54,24 @@ def download_specific_files(url, output_folder, filename):
                 raise e
     raise Exception
 
-
-# 1999-2000
-# file_dictionary = {
-#     'Demographics': ["DEMO.xpt"],
-#     "Examination": ["BPX.xpt", "BMX.xpt"],
-#     "Laboratory": ["LAB25.xpt", "LAB18.xpt", "LAB13.xpt", "LAB18.xpt", "LAB10.xpt"],
-#     "Questionnaire": ["PAQ.xpt", "DIQ.xpt", "MCQ.xpt"]
-# }
-
-# 2001-2002: B
-# file_dictionary = {
-#     'Demographics': ["DEMO_B.xpt"],
-#     "Examination": ["BPX_B.xpt", "BMX_B.xpt"],
-#     "Laboratory": ["L25_B.xpt", "L10_B.xpt", "L10_2_B.xpt", "L13_B.xpt", "L40_B.xpt", "L40_2_B.xpt"],
-#     "Questionnaire": ["PAQ_B.xpt", "DIQ_B.xpt", "MCQ_B.xpt"]
-# }
-
-# 2003-2004: C
-# file_dictionary = {
-#     'Demographics': ["DEMO_C.xpt"],
-#     "Examination": ["BPX_C.xpt", "BMX_C.xpt"],
-#     "Laboratory": ["L25_C.xpt", "L10_C.xpt", "L13_C.xpt", "L40_C.xpt"],
-#     "Questionnaire": ["PAQ_C.xpt", "DIQ_C.xpt", "MCQ_C.xpt"]
-# }
-
-
-# # 2005-2006: D
-# # 2007-2008: E
-# # 2009-2010: F
-# # 2011-2012: G
-# # 2013-2014: H
-# # 2015-2016: I
-# file_dictionary = {
-#     'Demographics': ["DEMO_I.xpt"],
-#     "Examination": ["BPX_I.xpt", "BMX_I.xpt"],
-#     "Laboratory": ["BIOPRO_I.xpt", "CBC_I.xpt", "GHB_I.xpt", "HDL_I.xpt", 'TCHOL_I.xpt'],
-#     "Questionnaire": ["PAQ_I.xpt", "DIQ_I.xpt", "MCQ_I.xpt"]
-# }
-
-
 def download_files_by_year_range(output_directory, year_range):
-    webpage_url = 'https://wwwn.cdc.gov/nchs/nhanes/search/datapage.aspx?Component={}&CycleBeginYear={}'
+    """
+    Downloads all associated files by year_range
+    
+    Args:
+        output_directory (str): The parent directory to save all the downloaded files
+        year_range (str): The year range of data to download
+    """
     start_year = int(year_range.split('-')[0])
     file_directories_path = 'file_directories.json'
     with open(file_directories_path, "r") as file:
         file_directory_dictionary = json.load(file)
 
     for directory, files in file_directory_dictionary[year_range].items():
-        print(f'Reading directory: "{directory}"')
+        print(f'Requesting directory: "{directory}"')
         formatted_url = webpage_url.format(directory, start_year)
         for file in files:
-            print(f' - Reading in file: "{file}"', end="")
+            print(f' - Downloading file: "{file}"', end="")
             try:
                 download_specific_files(formatted_url, output_directory, file)
                 print(' [SUCCESS]')
@@ -110,54 +80,48 @@ def download_files_by_year_range(output_directory, year_range):
 
 
 
-def check_attributes(directory, year_key):
-    file_path = "data_attributes.json"
+def validate_attributes(directory, year_range):
+    """
+    Checks for all valid attributes for the data interested in.
+    
+    Args:
+        directory (str): The parent directory where all the data files are located.
+        year_range (str): The year range of data to validate
+    """
+    print(f'Checking data extracted from year range: {year_range}')
 
-    print(f'Checking data extracted from year range: {year_key}')
     # Read the dictionary from the JSON file
-    with open(file_path, "r") as file:
+    with open(data_attributes_file_path, "r") as file:
         data_attribute_dict = json.load(file)
         
-    attribute_tracker = {attribute:0 for attribute in data_attribute_dict[year_key]}
+    attribute_tracker = {attribute:0 for attribute in data_attribute_dict[year_range]}
 
+    message = ''
     for file_name in os.listdir(directory):
-        try:
-            file_path = os.path.join(directory, file_name)
-
-            file_df = pd.read_sas(file_path, format='xport')
-            
-            for column in file_df.columns:
-                if column in attribute_tracker:
-                    attribute_tracker[column] += 1
-
-        except Exception as e:
-            print(' [FAILED]\n')
+        data_file_path = os.path.join(directory, file_name)
+        file_df = pd.read_sas(data_file_path, format='xport')
+        
+        for column in file_df.columns:
+            if column in attribute_tracker:
+                attribute_tracker[column] += 1
 
     for attribute, count in attribute_tracker.items():
         if count == 0:
-            print(f' - Attribute: {attribute} [MISSING]')
-    
-    print(f'Checking done.')
+            message += ' - Attribute: <' + attribute + '> [MISSING]\n'    
+    if len(message) == 0:
+        print('- All attributes checked [SUCCESS]')
+    else:
+        print(message.rstrip('\n'))
 
 
-
-output_directory = 'data_test'
-
-for start_year in [1999, 2001, 2003, 2005, 2007, 2009, 2011, 2013, 2015]:
+for start_year in start_years:
     year_range = str(start_year) + '-' + str(start_year + 1)
-    print(f'EXTRACTING {year_range} DATA')
+    print(f'~~~~EXTRACTING {year_range} DATA~~~~')
 
     output_file_path = f'{output_directory}/{year_range}'
     download_files_by_year_range(output_file_path, year_range)
-    check_attributes(output_file_path, year_range)
+    print()
+    validate_attributes(output_file_path, year_range)
 
-    print(f'DONE.\n')
+    print(f'~~~~DONE.~~~~\n')
 
-# check_attributes('data/2001_2002/', '2001-2002')
-# check_attributes('data/2003_2004/', '2003-2004')
-# check_attributes('data/2005_2006/', '2005-2006')
-# check_attributes('data/2007_2008/', '2007-2008')
-# check_attributes('data/2009_2010/', '2009-2010')
-# check_attributes('data/2011_2012/', '2011-2012')
-# check_attributes('data/2013_2014/', '2013-2014')
-# check_attributes('data/2015_2016/', '2015-2016')
