f1832c09b5b29a1ea6049ad1772cd2008ac21d33
Disilva019 <disilva0423@gmail.com>
2025-01-29 23:20:05 -0800

    Adversarial Module Work

--- Adversarial_Model_Module.py
+++ Adversarial_Model_Module.py
@@ -38,9 +38,7 @@ class AdversarialModel(keras.Model):
         self.optimizer = keras.optimizers.Adam(learning_rate=0.001)
 
         # Adversarial model (Gradient Boosted Trees)
-        self.adversarial_model = tfdf.keras.GradientBoostedTreesModel()(n_estimators=100, learning_rate=0.1)
-
-
+        self.adversarial_model = tfdf.keras.GradientBoostedTreesModel(n_estimators=100, learning_rate=0.1, task = tfdf.keras.Task.CLASSIFICATION)
 
     def call(self, inputs):
         """Forward pass"""
@@ -58,7 +56,6 @@ class AdversarialModel(keras.Model):
         main_model_train_loss_results = []
         combined_train_los_results = []
 
-            
         for epoch in range(self.epochs):
             epoch_loss_avg = tf.keras.metrics.Mean()
             epoch_accuracy = tf.keras.metrics.BinaryAccuracy()
@@ -70,25 +67,56 @@ class AdversarialModel(keras.Model):
 
             for X_train, y_train in data:
 
-                with tf.GradientTape() as tape:
-                    # Train Main Model
-                    # Forward pass
-                    y_pred = self(X_train, training=True)  
+                if self.epoch % self.GBT_retrain == 0:
+                    with tf.GradientTape() as tape:
 
-                    # Compute Main Model Loss
-                    main_model_loss = self.loss_fn(y_train, y_pred)
+                        # Train Main Model
+                        # Forward pass
+                        y_pred = self(X_train, training=True)  
 
-                    # Compute gradients
-                    trainable_vars = self.trainable_variables
-                    gradients = tape.gradient(main_model_loss, trainable_vars)
+                        # Compute Main Model Loss
+                        main_model_loss = self.loss_fn(y_train, y_pred)
 
-                    # Update weights
-                    self.optimizer.apply_gradients(zip(gradients, trainable_vars))
 
-                    # Track Model Progress
-                    epoch_loss_avg.update_state(main_model_loss)
-                    epoch_accuracy.update_state(y_train, y_pred)
+                        # Predict Sensitive Attribute
+                        adversarial_model = self.adversarial_model.fit(x = y_pred, y = z)
+                        
+                        # Compute Adversarial Model Loss
+                        adversarial_model_loss = adversarial_model.make_inspector().evaluation()[2]
 
+                        # Compute Combined Loss
+                        combined_loss = main_model_loss + (main_model_loss/adversarial_model_loss) - (self.lambda_tradeoff * adversarial_model_loss)
+                    
+                        # Compute gradients with Combined Loss
+                        trainable_vars = self.trainable_variables
+                        gradients = tape.gradient(combined_loss, trainable_vars)
+
+                        # Update weights
+                        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+
+                        # Track Model Progress
+                        epoch_loss_avg.update_state(combined_loss)
+                        epoch_accuracy.update_state(y_train, y_pred)
+
+                else:
+                    with tf.GradientTape() as tape:
+                        # Train Main Model
+                        # Forward pass
+                        y_pred = self(X_train, training=True)  
+
+                        # Compute Main Model Loss
+                        main_model_loss = self.loss_fn(y_train, y_pred)
+
+                        # Compute gradients
+                        trainable_vars = self.trainable_variables
+                        gradients = tape.gradient(main_model_loss, trainable_vars)
+
+                        # Update weights
+                        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+
+                        # Track Model Progress
+                        epoch_loss_avg.update_state(main_model_loss)
+                        epoch_accuracy.update_state(y_train, y_pred)
             
             # End epoch
             main_model_train_loss_results.append(epoch_loss_avg.result())
@@ -98,15 +126,12 @@ class AdversarialModel(keras.Model):
             progbar.update(epoch, values=[("accuracy", epoch_accuracy.result()), ("loss", epoch_loss_avg.result())])
 
             # Calculate elapsed time per step
-        elapsed_time = time.time() - start_time
-        time_per_epoch = elapsed_time / self.num_batches * 1e6  # Convert to microseconds
+            elapsed_time = time.time() - start_time
+            time_per_epoch = elapsed_time / self.num_batches * 1e6  # Convert to microseconds
 
-        print(f"{self.num_batches}/{self.num_batches} - {int(elapsed_time)}s {int(time_per_epoch)}us/step "
-          f"- accuracy: {epoch_accuracy.result():.4f} - loss: {epoch_loss_avg.result():.4f}")
+            print(f"{self.num_batches}/{self.num_batches} - {int(elapsed_time)}s {int(time_per_epoch)}us/step "
+            f"- accuracy: {epoch_accuracy.result():.4f} - loss: {epoch_loss_avg.result():.4f}")
 
 
-            # Train Adversarial Model
-            # Depending on Metric Input may Require 
-
-            if self.epoch % self.GBT_retrain == 0:
+        
                 
