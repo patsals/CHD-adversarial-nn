e237a8b50c3ad9fa8051b7e188122d6caa79e550
Disilva019 <disilva0423@gmail.com>
2025-01-30 18:14:32 -0800

    Fit Method Complete

--- Adversarial_Model_Module.py
+++ Adversarial_Model_Module.py
@@ -16,7 +16,7 @@ import time
 
 class AdversarialModel(keras.Model):
     def __init__(self, input_dim, sensitive_attr = str,lambda_tradeoff=0.1, metric = str, GBT_retrain = 5, epochs = 100, num_batches = 32):
-        super(AdversarialModel, self).__init__()
+        super().__init__()
 
         # Initialize Attributes
         self.lambda_tradeoff = lambda_tradeoff  # Trade-off parameter for adversarial penalty
@@ -33,17 +33,17 @@ class AdversarialModel(keras.Model):
         self.output_layer = Dense(1, activation='sigmoid')  # Binary classification
     
         
-        # Loss function and optimizer
+        # Loss function and optimizer for Main Model
         self.loss_fn = keras.losses.BinaryCrossentropy()
         self.optimizer = keras.optimizers.Adam(learning_rate=0.001)
 
         # Adversarial model (Gradient Boosted Trees)
         self.adversarial_model = tfdf.keras.GradientBoostedTreesModel(n_estimators=100, learning_rate=0.1, task = tfdf.keras.Task.CLASSIFICATION)
 
-    def call(self, inputs):
+    def call(self, inputs, train = False):
         """Forward pass"""
         x = self.dense1(inputs)
-        x = self.dropout1(x, training = True)
+        x = self.dropout1(x, training = train)
         x = self.dense2(x)
         return self.output_layer(x)
 
@@ -62,68 +62,65 @@ class AdversarialModel(keras.Model):
 
             # Epoch Progress Tracking
             start_time = time.time()
-            print(f"\nEpoch {epoch+1}/{self.epochs}")
+            print(f"\nEpoch {epoch + 1}/{self.epochs}")
             progbar = keras.utils.Progbar(target=self.num_batches)
 
-            for X_train, y_train in data:
+            if epoch % self.GBT_retrain == 0:
+                y_pred_full = []
+                y_train_full = []
 
-                if self.epoch % self.GBT_retrain == 0:
-                    with tf.GradientTape() as tape:
+                for X_train, y_train in data:
+                    y_pred_full.extend(self(X_train, train=False).numpy().flatten())
+                    y_train_full.extend(y_train.numpy().flatten())
 
-                        # Train Main Model
-                        # Forward pass
-                        y_pred = self(X_train, training=True)  
+                # Train the adversarial model on predictions vs sensitive attribute
+                self.adversarial_model.fit(x=y_pred_full, y=z)
 
-                        # Compute Main Model Loss
-                        main_model_loss = self.loss_fn(y_train, y_pred)
+                # Compute Adversarial Model Loss
+                adversarial_model_loss  = self.adversarial_model.make_inspector().evaluation()[2]
 
+            for batch_idx, (X_train, y_train) in enumerate(data):
 
-                        # Predict Sensitive Attribute
-                        adversarial_model = self.adversarial_model.fit(x = y_pred, y = z)
-                        
-                        # Compute Adversarial Model Loss
-                        adversarial_model_loss = adversarial_model.make_inspector().evaluation()[2]
+                with tf.GradientTape() as tape:
+                    # Forward pass
+                    y_pred = self(X_train, train=True)
 
+                    # Compute Main Model Loss
+                    main_model_loss = self.loss_fn(y_train, y_pred)
+
+                    if epoch % self.GBT_retrain == 0:
                         # Compute Combined Loss
-                        combined_loss = main_model_loss + (main_model_loss/adversarial_model_loss) - (self.lambda_tradeoff * adversarial_model_loss)
-                    
-                        # Compute gradients with Combined Loss
+                        combined_loss = main_model_loss + (main_model_loss / adversarial_model_loss) - (self.lambda_tradeoff * adversarial_model_loss)
+
+                        # Compute gradients
                         trainable_vars = self.trainable_variables
                         gradients = tape.gradient(combined_loss, trainable_vars)
 
                         # Update weights
                         self.optimizer.apply_gradients(zip(gradients, trainable_vars))
 
-                        # Track Model Progress
+                         # Track Model Progress
                         epoch_loss_avg.update_state(combined_loss)
                         epoch_accuracy.update_state(y_train, y_pred)
-
-                else:
-                    with tf.GradientTape() as tape:
-                        # Train Main Model
-                        # Forward pass
-                        y_pred = self(X_train, training=True)  
-
-                        # Compute Main Model Loss
-                        main_model_loss = self.loss_fn(y_train, y_pred)
-
-                        # Compute gradients
+                        
+                    else:
+                         # Compute gradients
                         trainable_vars = self.trainable_variables
                         gradients = tape.gradient(main_model_loss, trainable_vars)
 
                         # Update weights
                         self.optimizer.apply_gradients(zip(gradients, trainable_vars))
 
-                        # Track Model Progress
+                         # Track Model Progress
                         epoch_loss_avg.update_state(main_model_loss)
                         epoch_accuracy.update_state(y_train, y_pred)
-            
+
             # End epoch
             main_model_train_loss_results.append(epoch_loss_avg.result())
             combined_train_los_results.append(epoch_accuracy.result())
 
             # Update Progress Bar
-            progbar.update(epoch, values=[("accuracy", epoch_accuracy.result()), ("loss", epoch_loss_avg.result())])
+            progbar.update(1, values=[("accuracy", epoch_accuracy.result()), ("loss", epoch_loss_avg.result())])
 
             # Calculate elapsed time per step
             elapsed_time = time.time() - start_time
@@ -131,6 +128,22 @@ class AdversarialModel(keras.Model):
 
             print(f"{self.num_batches}/{self.num_batches} - {int(elapsed_time)}s {int(time_per_epoch)}us/step "
             f"- accuracy: {epoch_accuracy.result():.4f} - loss: {epoch_loss_avg.result():.4f}")
+
+            if epoch > 0 and epoch % 100 == 0:
+                self.save("/Saved_Models/Main_Model")
+                self.adversarial_model.save_model("/Saved_Models/Adversarial_Model")
+
+        # Save Final Neural Network Model
+        self.save("/Saved_Models/Final_Model")
+
+        return self
+
+
+    def evaluate(self, X, y):
+
+        loss_avg = tf.keras.metrics.Mean()
+        accuracy_metric = tf.keras.metrics.BinaryAccuracy()
+
 
 
         
